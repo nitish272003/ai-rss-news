a5065f92457c6b7926e3654001ea3f4008655b04
nitish272003 <158252059+nitish272003@users.noreply.github.com>
2025-07-21 12:25:39 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -6,6 +6,15 @@ class Config:
     TYPE = "service_account"
     PROJECT_ID = "eng-pulsar-466105-g5"
 
+    class LLM:
+        OPENROUTER_API_KEY = st.secrets["openrouter_api_key"]
+        OPENROUTER_API_URL = "https://openrouter.ai/api/v1"
+        OPENROUTER_MODEL = "perplexity/sonar"
+
+
+__all__ = ["Config", "rss_sources"]
+
+'''
     class Credentials:
         CLIENT_ID = "115510078425093279793"
         CLIENT_EMAIL = st.secrets["client_email"]
@@ -16,11 +25,5 @@ class Config:
         AUTH_PROVIDER_X509_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs"
         CLIENT_X509_CERT_URL = "https://www.googleapis.com/robot/v1/metadata/x509/the-neural-872%40eng-pulsar-466105-g5.iam.gserviceaccount.com"
         UNIVERSE_DOMAIN = "googleapis.com"
-
-    class LLM:
-        OPENROUTER_API_KEY = st.secrets["openrouter_api_key"]
-        OPENROUTER_API_URL = "https://openrouter.ai/api/v1"
-        OPENROUTER_MODEL = "perplexity/sonar"
-
-
-__all__ = ["Config", "rss_sources"]
+'''
+    
